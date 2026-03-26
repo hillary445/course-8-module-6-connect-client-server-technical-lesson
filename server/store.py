765b9a24cdcc4c3ef72038ed2d60ffe6df078f9e
hillary445 <hilaryprit02@gmail.com>
2026-03-26 14:37:02 +0300

    Connect front end to back end with get and POST

--- server/store.py
+++ server/store.py
@@ -1,6 +1,4 @@
-# TASK: Create a function called get_sample_data
-# This function should return a dictionary with keys "message" and "status"
-# For example: { "message": "Hello from Flask!", "status": "success" }
+data = {"message": "Hello from Flask!", "status": "success"}
 
-# def get_sample_data():
-#     pass
+def get_sample_data():
+    return data
